494c7865f668c2e8b5508ce8ea7c5fa7bb38d905
John Helder <johnhelderjp@gmail.com>
2022-11-21 21:11:06 -0300

    Criando as funções do item

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,12 @@ document.querySelectorAll('.item').forEach(item => {
   item.addEventListener('dragstart', dragStart);
   item.addEventListener('dragend', dragEnd);
 });
+
+//Function item
+function dragStart(e){
+  e.currentTarget.classList.add('dragging');
+}
+
+function dragEnd(e){
+  e.currentTarget.classList.remove('dragging')
+}
